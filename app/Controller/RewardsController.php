bc9865b4233ddf79cfb042694b77ca05af74d314
stefany hurtado <stefyhurtado@gmail.com>
2013-12-15 22:17:30 -0300

    Cambios en el rewards

--- app/Controller/RewardsController.php
+++ app/Controller/RewardsController.php
@@ -14,6 +14,11 @@ class RewardsController extends AppController {
  * @var array
  */
 	public $components = array('Paginator');
+     public function beforeFilter() {   
+    	parent::beforeFilter();
+		$this->Auth->allow('index', 'view');
+	}
+
 
 /**
  * index method
